cd7081e6216fc637ef45e65cff262a4f6ac7f6dc
minidonut <nycom13@gmail.com>
2020-08-28 02:51:49 +0900

    add from gist function

--- packages/ts-app/src/index.ts
+++ packages/ts-app/src/index.ts
@@ -1,5 +1,22 @@
 import * as fs from 'fs';
 import * as path from 'path';
+import axios from 'axios';
+
+// prettier-ignore
+const gistUrls = {
+  'package.json': 'https://gist.githubusercontent.com/minidonut/84cf4f084328f2044d26c3b849d0fada/raw/0318a4e86407250bf2a57a42be6bad835c43cb2b/package.json-template-1',
+
+
+} as const;
+
+const fromRemote = <T extends Record<string, string>>(urls: T) => async (key: keyof T): Promise<string> => {
+  const url = urls[key];
+  console.log(`download ${url.slice(0, 64)}...`);
+  const { data } = await axios.get(url);
+  return JSON.stringify(data, undefined, 2);
+};
+
+const fromGist = fromRemote(gistUrls);
 
 const touch = (filename: string) => {
   const absolutePath = path.resolve(filename);
@@ -7,18 +24,19 @@ const touch = (filename: string) => {
   fs.mkdirSync(dirname, { recursive: true });
   fs.writeFileSync(absolutePath, '');
 
-  return {
-    pipe: (data: string | Promise<string>): void | Promise<void> => {
-      if (typeof data === 'string') {
-        fs.writeFileSync(absolutePath, data);
-      } else {
-        return data.then(d => fs.writeFileSync(absolutePath, d));
-      }
-    },
+  const text = (data: string): void => {
+    fs.writeFileSync(absolutePath, data);
   };
+
+  const gist = async (key: Parameters<typeof fromGist>[0]) => {
+    const data = await fromGist(key);
+    fs.writeFileSync(absolutePath, data);
+  };
+
+  return { text, gist };
 };
 
-(function () {
+(async function () {
   const name = process.argv[2];
   if (name == null) {
     console.error(`specify project name`);
@@ -27,6 +45,6 @@ const touch = (filename: string) => {
   fs.mkdirSync(name);
   process.chdir(name);
 
-  touch('src/index.ts').pipe(`console.log('hello world!')`);
-  touch('package.json').pipe('{}');
+  touch('src/index.ts').text(`console.log('hello world!')`);
+  await touch('package.json').gist('package.json');
 })();
