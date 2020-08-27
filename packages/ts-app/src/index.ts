e9a23d8fb951702f4afda35127268375f96485f5
minidonut <nycom13@gmail.com>
2020-08-28 01:56:58 +0900

    add touch function

--- packages/ts-app/src/index.ts
+++ packages/ts-app/src/index.ts
@@ -1,10 +1,32 @@
-import { mkdir } from 'common';
+import * as fs from 'fs';
+import * as path from 'path';
+
+const touch = (filename: string) => {
+  const absolutePath = path.resolve(filename);
+  const dirname = path.dirname(filename);
+  fs.mkdirSync(dirname, { recursive: true });
+  fs.writeFileSync(absolutePath, '');
+
+  return {
+    pipe: (data: string | Promise<string>): void | Promise<void> => {
+      if (typeof data === 'string') {
+        fs.writeFileSync(absolutePath, data);
+      } else {
+        return data.then(d => fs.writeFileSync(absolutePath, d));
+      }
+    },
+  };
+};
+
 (function () {
   const name = process.argv[2];
   if (name == null) {
     console.error(`specify project name`);
     process.exit(1);
   }
+  fs.mkdirSync(name);
+  process.chdir(name);
 
-  mkdir('src/template');
+  touch('src/index.ts').pipe(`console.log('hello world!')`);
+  touch('package.json').pipe('{}');
 })();
