9bd3c16dcf4315bc69ce6ae7befedeb0911abe4e
minidonut <nycom13@gmail.com>
2020-08-28 00:47:51 +0900

    add help subcommand

---
+++ packages/router/src/help.ts
@@ -0,0 +1,8 @@
+console.log(`
+usage: create <command> [<args>]
+
+scaffold and initialize various type of application written in typescript
+
+basic template
+   ts-app      // todo auto generate here
+`);
