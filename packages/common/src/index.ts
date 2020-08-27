040c68d624709e478c4c05526d2568e505a8134a
minidonut <nycom13@gmail.com>
2020-08-28 01:38:59 +0900

    add common utils "ls", "mkdir" working recursive

--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -1,5 +1,39 @@
+import * as fs from 'fs';
+import * as path from 'path';
 const test = () => {
   console.log('it is common packages');
 };
 
-export { test };
+const ls = (path: string, type: 'file' | 'directory' | 'all' = 'all') =>
+  fs
+    .readdirSync(path, { withFileTypes: true })
+    .filter(f => (type === 'file' ? f.isFile() : type === 'directory' ? f.isDirectory() : true))
+    .map(f => f.name);
+
+const mkdir = (name: string) => {
+  let base = process.cwd();
+  const paths = name
+    .replace(/^\./, '')
+    .split('/')
+    .filter(x => x !== '')
+    .reverse();
+
+  while (paths.length) {
+    const name = paths.pop();
+    if (name == null) break;
+
+    const files = ls(base, 'file');
+    const dirs = ls(base, 'directory');
+
+    if (files.includes(name)) {
+      throw new Error(`cannot create ${name}. already exist`);
+    } else {
+      base = path.join(base, name);
+      if (!dirs.includes(name)) {
+        fs.mkdirSync(base);
+      }
+    }
+  }
+};
+
+export { test, ls, mkdir };
--- packages/ts-app/src/index.ts
+++ packages/ts-app/src/index.ts
@@ -1,1 +1,10 @@
-console.log(process.argv);
+import { mkdir } from 'common';
+(function () {
+  const name = process.argv[2];
+  if (name == null) {
+    console.error(`specify project name`);
+    process.exit(1);
+  }
+
+  mkdir('src/template');
+})();
