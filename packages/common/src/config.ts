fbbb49c76d79faa7335b68ea06278c6546fd84f0
minidonut <nycom13@gmail.com>
2020-08-29 14:00:01 +0900

    add logs when load config

--- packages/common/src/config.ts
+++ packages/common/src/config.ts
@@ -1,12 +1,15 @@
 import * as os from 'os';
 import * as path from 'path';
+import * as chalk from 'chalk';
 import * as fs from 'fs-extra';
 
 const CONFIG_PATH = path.join(os.homedir(), '.config', '.create-app.json');
 let cache: Record<string, string> | null = null;
 
 const load = () => {
+  console.log(chalk.gray(`load config from ${CONFIG_PATH}`));
   if (!fs.existsSync(CONFIG_PATH)) {
+    console.log(chalk.yellow('warning'), "config doesn't exist");
     fs.outputFileSync(CONFIG_PATH, '{}');
   }
 
