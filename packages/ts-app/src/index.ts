e411008e58574c19a1996a7eb42607e16c793cd4
minidonut <nycom13@gmail.com>
2020-08-29 15:30:25 +0900

    complete first iteration 'ts-app'

--- packages/ts-app/src/index.ts
+++ packages/ts-app/src/index.ts
@@ -1,9 +1,4 @@
 import { init, fromRoot, file, greet, context, config } from 'common';
-// prettier-ignore
-const gistUrls = {
-  'package.json': {id: "84cf4f084328f2044d26c3b849d0fada", filename: 'package.json-template-1' },
-} as const;
-
 (async function () {
   const name = process.argv[2];
   if (name == null) {
@@ -19,7 +14,14 @@ const gistUrls = {
   fromRoot.create(name);
 
   await file('src/index.ts').fromText(`console.log("hello ${name}");`);
-  await file('package.json').fromGist(gistUrls['package.json']);
+  await file('package.json').fromGist({ id: '84cf4f084328f2044d26c3b849d0fada', filename: 'package.json-template-1' });
+  await file('.editorconfig').fromGist('15626a36577e771d8323565cab3e4c63');
+  await file('.eslintignore').fromGist('15626a36577e771d8323565cab3e4c63');
+  await file('.eslintrc.js').fromGist('15626a36577e771d8323565cab3e4c63');
+  await file('.gitignore').fromGist('15626a36577e771d8323565cab3e4c63');
+  await file('.prettierrc.json').fromGist('15626a36577e771d8323565cab3e4c63');
+  await file('license').fromGist('15626a36577e771d8323565cab3e4c63');
+  await file('tsconfig.json').fromGist('15626a36577e771d8323565cab3e4c63');
 
   init.yarn();
   init.git({ initialCommit: true });
