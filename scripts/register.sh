ac8fcea3d617e87dd4ac120f355cab43b9abae50
minidonut <nycom13@gmail.com>
2021-01-09 21:29:01 +0900

    Add more package to register script

--- scripts/register.sh
+++ scripts/register.sh
@@ -10,6 +10,9 @@ yarn workspace router link
 yarn workspace ts-app build
 yarn workspace ts-app link
 
+yarn workspace ts-mono build
+yarn workspace ts-mono link
+
 yarn workspace js-app build
 yarn workspace js-app link
 
