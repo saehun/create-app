4af1dc4fdc2f12419d505f738452a33d0a235d61
minidonut <nycom13@gmail.com>
2021-01-09 21:30:01 +0900

    make create-ts-mono works

--- packages/ts-mono/src/index.ts
+++ packages/ts-mono/src/index.ts
@@ -1,6 +1,6 @@
 import { init, fromRoot, file, greet, context, config } from 'common';
 import { editorconfig, eslintignore, eslintrc, gitignore, jestConfig, license, prettierc, tsconfig } from 'template';
-import { indexTestTs, packageJson } from './templates';
+import { indexTestTs, lernaJson, packageJson, rootPackageJson } from './templates';
 (async function () {
   const name = process.argv[2];
   if (name == null) {
@@ -8,28 +8,32 @@ import { indexTestTs, packageJson } from './templates';
     process.exit(1);
   }
 
+  const packageName = 'common';
+
   // inject local config to context
   context(config());
   context.set('project-name', name);
+  context.set('package-name', packageName);
 
   // create base directory
   fromRoot.create(name);
 
-  await file('src/index.ts').fromText(`console.log("hello ${name}");`);
-  await file('src/index.test.ts').fromText(indexTestTs);
-  await file('package.json').fromText(packageJson);
+  await file(`packages/${packageName}/src/index.ts`).fromText(`console.log("hello ${name}");`);
+  await file(`packages/${packageName}/src/index.test.ts`).fromText(indexTestTs);
+  await file(`packages/${packageName}/package.json`).fromText(packageJson);
+  await file(`packages/${packageName}/jest.config.js`).fromText(jestConfig.typescript);
+  await file(`packages/${packageName}/license`).fromText(license.MIT);
+  await file('lerna.json').fromText(lernaJson);
+  await file('package.json').fromText(rootPackageJson);
+  await file('tsconfig.json').fromText(tsconfig.simpleNode);
   await file('.editorconfig').fromText(editorconfig);
   await file('.eslintignore').fromText(eslintignore.base);
   await file('.eslintrc.js').fromText(eslintrc.typescript);
   await file('.gitignore').fromText(gitignore.base);
   await file('.prettierrc.json').fromText(prettierc.base);
-  await file('license').fromText(license.MIT);
-  await file('tsconfig.json').fromText(tsconfig.simpleNode);
-  await file('jest.config.js').fromText(jestConfig.typescript);
 
   init.yarn();
   init.git({ initialCommit: true });
-  init.nextCommand(`cd ${name} && yarn start`);
 
   greet.happyHacking();
 })();
--- packages/ts-mono/src/templates/index.ts
+++ packages/ts-mono/src/templates/index.ts
@@ -1,10 +1,43 @@
-export const packageJson = `{
+export const rootPackageJson = `{
   "name": "{{project-name}}",
+  "private": true,
+  "description": "",
+  "license": "MIT",
+  "repository": "{{git-user-id}}/{{project-name}}",
+  "author": {
+    "name": "{{git-user-name}}",
+    "email": "{{git-user-email}}"
+  },
+  "workspaces": [
+    "packages/*"
+  ],
+  "devDependencies": {
+    "@types/jest": "^26.0.20",
+    "@types/node": "^14.14.20",
+    "@typescript-eslint/eslint-plugin": "^4.12.0",
+    "@typescript-eslint/parser": "^4.12.0",
+    "eslint": "^7.17.0",
+    "jest": "^26.6.3",
+    "prettier": "^2.2.1",
+    "ts-jest": "^26.4.4",
+    "ts-node": "^9.1.1",
+    "typescript": "^4.1.3",
+    "lerna": "^3.22.1"
+  }
+}`;
+
+export const packageJson = `{
+  "name": "{{package-name}}",
   "version": "1.0.0",
   "description": "",
   "license": "MIT",
   "main": "dist/index.js",
-  "repository": "{{git-user-id}}/{{project-name}}",
+  "types": "dist/index.d.ts",
+  "repository": {
+    "type": "git",
+    "url": "https://github.com/{{git-user-id}}/{{project-name}}", 
+    "directory": "packages/{{package-name}}"
+  }, 
   "scripts": {
     "build": "tsc",
     "test": "NODE_ENV=test jest",
@@ -13,22 +46,18 @@ export const packageJson = `{
     "test:debug": "node --inspect-brk ./node_modules/jest/bin/jest.js --runInBand",
     "start:debug": "node --require ts-node/register --inspect-brk src/index.ts"
   },
-  "author": {
-    "name": "{{git-user-name}}",
-    "email": "{{git-user-email}}"
-  },
-  "devDependencies": {
-    "@types/jest": "^26.0.15",
-    "@types/node": "^14.14.10",
-    "@typescript-eslint/eslint-plugin": "^4.8.2",
-    "@typescript-eslint/parser": "^4.8.2",
-    "eslint": "^7.14.0",
-    "jest": "^26.6.3",
-    "prettier": "^2.2.0",
-    "ts-jest": "^26.4.4",
-    "ts-node": "^9.0.0",
-    "typescript": "^4.1.2"
-  }
+  "files": [
+    "license",
+    "package.json",
+    "dist/"
+  ]
+}`;
+
+export const lernaJson = `{
+  "packages": ["packages/*"],
+  "version": "independent",
+  "useWorkspaces": true,
+  "npmClient": "yarn"
 }`;
 
 export const indexTestTs = `test('pass', () => {
